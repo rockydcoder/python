3c4c0ee5dc874d5214fd80c48658a3df6f322c88
rockydocoder <rakesh.rockyrocks.xxx@gmail.com>
2015-12-06 11:37:09 +0530

    06/12/2015

---
+++ driver_exam.py
@@ -0,0 +1,19 @@
+answers = ['A', 'C', 'A', 'A', 'D', 'B', 'C', 'A', 'C', 'B', 'A', 'D', 'C', 'A', 'D', 'C', 'B', 'B', 'D', 'A']
+f = open('answers.txt', 'r')
+response = []
+incorrect = []
+for line in f:
+    response.append(line[0])
+counter = 0
+for i in range(0,19):
+    if answers[i] == response[i]:
+        counter+=1
+    else:
+        incorrect.append(i+1)
+if counter >= 15:
+    print "Driver has passed"
+else:
+    print "Driver has failed"
+print "Number of correct questions = ", counter
+print "Number of incorrect questions = ", (20-counter)
+print incorrect
